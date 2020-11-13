5302d66ed0b49a4dfd6e662e9c563d29bb0dec14
elducati <geomondi09@gmail.com>
2020-11-13 03:31:34 +0300

    ...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React, {useState, useEffect} from "react";
-import Locate from "./components/Locate";
-import Search from "./components/Search"
-import restaurant from "./restaurant.svg"
+import React from "react";
 import {
   GoogleMap,
   useLoadScript,
   Marker,
   InfoWindow,
 } from "@react-google-maps/api";
-
-
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
 import { formatRelative } from "date-fns";
 
 import "@reach/combobox/styles.css";
 import mapStyles from "./mapStyles";
-import Axios from "axios";
 
 const libraries = ["places"];
 const mapContainerStyle = {
   height: "100vh",
-  width: "75vw",
+  width: "100vw",
 };
 const options = {
   styles: mapStyles,
@@ -27,21 +32,18 @@ const options = {
   zoomControl: true,
 };
 const center = {
-  lat: -1.285790,
-  lng: 36.820030,
+  lat: 43.6532,
+  lng: -79.3832,
 };
-const googleMapsApiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY
-const App = () => {
+
+export default function App() {
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
     libraries,
   });
-  // markers state
   const [markers, setMarkers] = React.useState([]);
   const [selected, setSelected] = React.useState(null);
-  //latitude and longitude states
-  const [latitude, setLatitude] = useState(0)
-  const [longitude, setLogitude] = useState(0)
+
   const onMapClick = React.useCallback((e) => {
     setMarkers((current) => [
       ...current,
@@ -52,20 +54,7 @@ const App = () => {
       },
     ]);
   }, []);
-  //restaurants state
-  const [restaurants, setRestaurants ] = useState([])
-   
-   useEffect(() => {
-    console.log('effect')
-    
-    Axios
-      .get(`https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=1500&type=restaurant&key=${googleMapsApiKey}`)
-      .then(response => {
-        let restaurants = response.data.results
-        console.log(restaurants)
-        setRestaurants(restaurants)
-      })
-  }, [latitude, longitude])
+
   const mapRef = React.useRef();
   const onMapLoad = React.useCallback((map) => {
     mapRef.current = map;
@@ -82,7 +71,7 @@ const App = () => {
   return (
     <div>
       <h1>
-        Restaurants{" "}
+        Bears{" "}
         <span role="img" aria-label="tent">
           ⛺️
         </span>
@@ -104,14 +93,14 @@ const App = () => {
           <Marker
             key={`${marker.lat}-${marker.lng}`}
             position={{ lat: marker.lat, lng: marker.lng }}
-            onLoad={() => {
+            onClick={() => {
               setSelected(marker);
             }}
             icon={{
-              url: restaurant,
+              url: `/bear.svg`,
               origin: new window.google.maps.Point(0, 0),
               anchor: new window.google.maps.Point(15, 15),
-              scaledSize: new window.google.maps.Size(50, 50),
+              scaledSize: new window.google.maps.Size(30, 30),
             }}
           />
         ))}
@@ -135,9 +124,82 @@ const App = () => {
           </InfoWindow>
         ) : null}
       </GoogleMap>
-
     </div>
   );
 }
-export default App
 
+function Locate({ panTo }) {
+  return (
+    <button
+      className="locate"
+      onClick={() => {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            panTo({
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            });
+          },
+          () => null
+        );
+      }}
+    >
+      <img src="/compass.svg" alt="compass" />
+    </button>
+  );
+}
+
+function Search({ panTo }) {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      location: { lat: () => 43.6532, lng: () => -79.3832 },
+      radius: 100 * 1000,
+    },
+  });
+
+  // https://developers.google.com/maps/documentation/javascript/reference/places-autocomplete-service#AutocompletionRequest
+
+  const handleInput = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleSelect = async (address) => {
+    setValue(address, false);
+    clearSuggestions();
+
+    try {
+      const results = await getGeocode({ address });
+      const { lat, lng } = await getLatLng(results[0]);
+      panTo({ lat, lng });
+    } catch (error) {
+      console.log("😱 Error: ", error);
+    }
+  };
+
+  return (
+    <div className="search">
+      <Combobox onSelect={handleSelect}>
+        <ComboboxInput
+          value={value}
+          onChange={handleInput}
+          disabled={!ready}
+          placeholder="Search your location"
+        />
+        <ComboboxPopover>
+          <ComboboxList>
+            {status === "OK" &&
+              data.map(({ id, description }) => (
+                <ComboboxOption key={id} value={description} />
+              ))}
+          </ComboboxList>
+        </ComboboxPopover>
+      </Combobox>
+    </div>
+  );
+}
