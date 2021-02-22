e0ea592e46a8c7114d00d5e4bb5b8829dbdecd52
elducati <geomondi09@gmail.com>
2021-02-22 07:08:17 +0300

    modified the appBar

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -2,6 +2,7 @@ import React from "react"
 import AppBar from "@material-ui/core/AppBar"
 import Toolbar from "@material-ui/core/Toolbar"
 import Typography from "@material-ui/core/Typography"
+import compass from "../compass.svg"
 
 const NavBar = () => {
     return(
@@ -11,6 +12,7 @@ const NavBar = () => {
                     <Typography color="inherit">
                         Restaurants Review
                     </Typography>
+                    <img src={compass} alt="Current Location" height="50" style={{ float: "right", cursor: "pointer" }} onClick={() => window.location.reload(false)} />
                 </Toolbar>
             </AppBar>
             
